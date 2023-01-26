24bb4586a3c84108fa3785d362c5c8838e831ef4
javmarti <martingallegojavier@outlook.com>
2023-01-26 23:42:01 +0100

    :)

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: javmarti <javmarti@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 16:46:31 by javmarti          #+#    #+#             */
-/*   Updated: 2023/01/26 23:16:58 by javmarti         ###   ########.fr       */
+/*   Updated: 2023/01/26 23:41:46 by javmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int		create_child(t_pipex pipex);
 t_pipex	read_input(int argc, char *argv[], char *envp[]);
+void	free_pipex(t_pipex pipex);
+void	close_fds(t_pipex pipex);
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -25,23 +27,36 @@ int	main(int argc, char *argv[], char *envp[])
 	pipex = read_input(argc, argv, envp);
 	if ((access(pipex.outfile, F_OK) == 0 && access(pipex.outfile, W_OK) == -1)
 		|| (pipex.heredoc_flag == 0 && access(argv[1], R_OK) == -1))
+	{
+		free_pipex(pipex);
 		perror_exit("access error", 0);
+	}
 	while (++pipex.index < pipex.comm_number)
 	{
 		status = create_child(pipex);
 		if (status < 0)
-			perror_exit("child error", status);
-		if (pipex.index == 0)
-			close(pipex.pipes_fd[0][WRITE_END]);
-		else if (pipex.index == pipex.comm_number - 1)
-			close(pipex.pipes_fd[pipex.index - 1][READ_END]);
-		else
 		{
-			close(pipex.pipes_fd[pipex.index - 1][READ_END]);
-			close(pipex.pipes_fd[pipex.index][WRITE_END]);
+			free_pipex(pipex);
+			perror_exit("child error", status);
 		}
+		close_fds(pipex);
 	}
+	free_pipex(pipex);
 	exit(status);
+}
+
+void	close_fds(t_pipex pipex)
+{
+	if (pipex.index == 0)
+		close(pipex.pipes_fd[0][WRITE_END]);
+	else if (pipex.index == pipex.comm_number - 1)
+		close(pipex.pipes_fd[pipex.index - 1][READ_END]);
+	else
+	{
+		close(pipex.pipes_fd[pipex.index - 1][READ_END]);
+		close(pipex.pipes_fd[pipex.index][WRITE_END]);
+	}
+	return ;
 }
 
 int	**get_pipes_fd(int count)
@@ -49,7 +64,7 @@ int	**get_pipes_fd(int count)
 	int	**pipes_fd;
 	int	index;
 
-	pipes_fd = (int **)ft_calloc(count, sizeof(int *));
+	pipes_fd = (int **)ft_calloc(count + 1, sizeof(int *));
 	if (pipes_fd == NULL)
 		perror_exit("Error pipe", 1);
 	index = 0;
@@ -87,4 +102,14 @@ t_pipex	read_input(int argc, char *argv[], char *envp[])
 		perror_exit("malloc get paths error", 0);
 	pipex.index = -1;
 	return (pipex);
+}
+
+void	free_pipex(t_pipex pipex)
+{
+	int	index;
+
+	index = -1;
+	while (pipex.pipes_fd[++index])
+		free(pipex.pipes_fd[index]);
+	free(pipex.pipes_fd);
 }
